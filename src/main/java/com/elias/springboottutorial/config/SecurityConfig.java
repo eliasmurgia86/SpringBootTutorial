8d6f91c3ce6349656ee5251e5df02b047d3a8fca
Elias <e.murgia@contractors.crifgroup.com>
2022-12-20 12:30:08 +0100

    Spring boot security auto-configuration customization

--- src/main/java/com/elias/springboottutorial/config/SecurityConfig.java
+++ src/main/java/com/elias/springboottutorial/config/SecurityConfig.java
@@ -1,21 +1,49 @@
 package com.elias.springboottutorial.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService(PasswordEncoder passoordEncoder){
+        UserDetails user = User.withUsername("user")
+                .password(passoordEncoder.encode("password"))
+                .roles("USER")
+                .build();
+
+        UserDetails admin = User.withUsername("admin")
+                .password(passoordEncoder.encode("admin"))
+                .roles("USER", "ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(user, admin);
+    }
+
+    @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
         http.authorizeRequests()
                 .anyRequest()
-                .permitAll()
+                .authenticated()
                 .and()
-                .csrf()
-                .disable();
+                .httpBasic();
+
         return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        return  encoder;
     }
 }
