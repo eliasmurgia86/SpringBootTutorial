7237b133b169c28e08e185958b56ecd4417baecc
Elias <e.murgia@contractors.crifgroup.com>
2022-12-20 09:53:34 +0100

    Added credentials to login

--- src/main/java/com/elias/springboottutorial/config/SecurityConfig.java
+++ src/main/java/com/elias/springboottutorial/config/SecurityConfig.java
@@ -1,6 +1,5 @@
 package com.elias.springboottutorial.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -10,7 +9,6 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class SecurityConfig {
 
-    @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
         http.authorizeRequests()
                 .anyRequest()
